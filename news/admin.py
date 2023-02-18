f125293bf3434fd651610f622b3a5f05d6439b02
паша <13>
2023-02-18 09:06:45 +0500

    маялся с админкой

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from news.models import Img
+
+
 # Register your models here.
+@admin.register(Img)
+class ImgAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date',)
+    readonly_fields = ('date',)
+    list_filter = ('date',)
+    search_fields = ('title', )
+    fields = ('title', 'date', 'img',)
+
+
+
+
--- news/models.py
+++ news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Img(models.Model):
+    title = models.CharField(max_length=32, verbose_name='название')
+    img = models.ImageField(upload_to='%Y/%m/%d/', verbose_name='картинка')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='дата')
+
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name='картинка'
+        verbose_name_plural='картинки'
+        ordering=['date', 'title']
+
+
