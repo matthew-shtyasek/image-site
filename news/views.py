8a0849b5240617420af498c2e10f1d47880f14af
паша <13>
2023-02-18 11:44:26 +0500

    Добавили обработчик для сохранения картинок

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
-from news.forms import CommentForm
+from news.forms import CommentForm, ImageForm
 from news.models import Img
 
 
@@ -29,4 +30,14 @@ def news_detail_view(request, img_id):
     return render(request, 'news/details.html', context)
 
 
+def save_image_view(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('news:img'))  # слева пространство имён, справа  название ссылки
+    else:
+        form = ImageForm()
 
+    context = {'form': form}
+    return render(request, 'news/save_image.html', context)
