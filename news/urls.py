5dd3d079bda7de4a596d66387bef602c64f82fb2
паша <13>
2023-02-18 11:46:59 +0500

    прописан путь до обработчика, вёрстка формы тегами <p>

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from news.views import news_views, news_detail_view
+from news.views import news_views, news_detail_view, save_image_view
 
 app_name = 'news'
 urlpatterns = [
     path('post/<int:img_id>/', news_detail_view, name='detail'),
     path('', news_views, name='img'),
-
-
+    path('save-image/', save_image_view, name='save_image'),
 ]
 
