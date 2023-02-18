e022d10e42cf6312efe81931c442d98a21a40245
паша <13>
2023-02-18 10:19:43 +0500

    Создали обработчик и шаблон detail, починили thumbnail

--- news/views.py
+++ news/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from news.models import Img
 
 
 def news_views(request):
-    context = {'img': Img.objects.all() }
+    context = {'imgs': Img.objects.all() }
     return render(request, 'news/page_1.html', context)
+
+
+def news_detail_view(request, img_id):
+    detail = get_object_or_404(Img, id=img_id)
+    context = {'img_detail': detail}
+    return render(request, 'news/details.html', context)
