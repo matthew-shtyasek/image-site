6c384e8dfc851bcf3beab521cf0e02b9eaa14cc2
паша <13>
2023-02-18 11:36:18 +0500

    Создали форму для картинки

--- news/forms.py
+++ news/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from news.models import Comment
+from news.models import Comment, Img
 
 
 class CommentForm(forms.ModelForm):
@@ -8,3 +8,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text')
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Img
+        fields = ('title', 'img')
